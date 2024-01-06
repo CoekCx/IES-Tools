89992cbf59afe490cf4a43c20b00028dc6afc3c1
CoekCx <nemanja.ja.jovicic7@gmail.com>
2024-01-06 22:35:36 +0100

    added data and constants

---
+++ constants/constants.py
@@ -0,0 +1,2 @@
+IES_FOLDER_PATH = "D:\\Coek\\FTN\\7. Semestar\\Integracija elektroenergetskih sistema"
+DATA_PATH = "D:\\Coek\\FTN\\7. Semestar\\Integracija elektroenergetskih sistema\\Stock Analizer\\data"
